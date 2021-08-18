34c2bb1bd56b087e50122490026b66d74bc9595f
Mirmostafa <mirmostafa@hotmail.com>
2021-08-18 20:27:39 +0430

    Added code coverage for CodeRunner

--- Library/CodeRunner.cs
+++ Library/CodeRunner.cs
@@ -1,18 +1,16 @@
-﻿namespace Library
+﻿namespace Library;
+public class CodeRunner
 {
-    public class CodeRunner
+    private readonly List<Action> _Statements = new();
+
+    public static CodeRunner StartWith(Action statement)
     {
-        private readonly List<Action> _Statements = new List<Action>();
-
-        public static CodeRunner StartWith(Action statement)
-        {
-            var result = new CodeRunner();
-            result._Statements.Add(statement);
-            return result;
-        }
-
-        public CodeRunner Then(Action statement) => this.Fluent(() => this._Statements.Add(statement));
-
-        public CodeRunner Run() => ForEach(this, _Statements, statement => statement?.Invoke());
+        var result = new CodeRunner();
+        result._Statements.Add(statement);
+        return result;
     }
+
+    public CodeRunner Then(Action statement) => this.Fluent(() => this._Statements.Add(statement));
+
+    public CodeRunner Run() => ForEach(this, this._Statements, statement => statement?.Invoke());
 }
---
+++ Tests/LibraryTest/CodeRunnerTest.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+
+namespace LibraryTest;
+[TestClass]
+public class CodeRunnerTest
+{
+    [TestMethod]
+    public void CodeTest()
+    {
+        var i = 0;
+        void add() => i++;
+        CodeRunner
+            .StartWith(add)
+            .Then(add)
+            .Then(add)
+            .Then(add)
+            .Then(add)
+            .Run();
+        Assert.AreEqual(5, i);
+    }
+}
