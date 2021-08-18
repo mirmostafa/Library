10893f780b2d7d7799fe90c5746bca2dd1132a4c
Mirmostafa <mirmostafa@hotmail.com>
2021-08-18 23:05:02 +0430

    Code clean-up

--- Library/EmptyDisposable.cs
+++ Library/EmptyDisposable.cs
@@ -1,25 +1,23 @@
-﻿namespace Library
+﻿namespace Library;
+public class EmptyDisposable : IDisposable
 {
-    public class EmptyDisposable : IDisposable
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
     {
-        private bool disposedValue;
-
-        protected virtual void Dispose(bool disposing)
+        if (!this.disposedValue)
         {
-            if (!this.disposedValue)
+            if (disposing)
             {
-                if (disposing)
-                {
-                }
-                this.disposedValue = true;
             }
+            this.disposedValue = true;
         }
+    }
 
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            this.Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
