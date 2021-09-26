28be3f13164b6cccfc263eaa9e514d323df9e300
Mirmostafa <mirmostafa@hotmail.com>
2021-09-26 13:31:55 +0330

    WebResult tests writing is done.

--- Tests/LibraryTest/StringHelperTest.cs
+++ Tests/LibraryTest/StringHelperTest.cs
@@ -1,101 +1,100 @@
-namespace LibraryTest
+namespace LibraryTest;
+
+[TestClass]
+public class StringHelperTest
 {
-    [TestClass]
-    public class StringHelperTest
+    private const string text = "There 'a text', inside 'another text'. I want 'to find\" it.";
+
+    [TestMethod]
+    public void IsNullOrEmptyTest()
     {
-        private const string text = "There 'a text', inside 'another text'. I want 'to find\" it.";
+        var emptyStr = string.Empty;
+        var sampleStr = "Sample String";
 
-        [TestMethod]
-        public void IsNullOrEmptyTest()
-        {
-            var emptyStr = string.Empty;
-            var sampleStr = "Sample String";
+        var emptyStrIsEmpty = emptyStr.IsNullOrEmpty();
+        var sampleStrIsEmpty = sampleStr.IsNullOrEmpty();
 
-            var emptyStrIsEmpty = emptyStr.IsNullOrEmpty();
-            var sampleStrIsEmpty = sampleStr.IsNullOrEmpty();
+        Assert.IsTrue(emptyStrIsEmpty);
+        Assert.IsFalse(sampleStrIsEmpty);
+    }
 
-            Assert.IsTrue(emptyStrIsEmpty);
-            Assert.IsFalse(sampleStrIsEmpty);
-        }
+    [TestMethod]
+    public void IsNumberTest()
+    {
+        var numStr = "123456789";
+        var numStrIsNumbber = numStr.IsNumber();
 
-        [TestMethod]
-        public void IsNumberTest()
-        {
-            var numStr = "123456789";
-            var numStrIsNumbber = numStr.IsNumber();
+        var invNumStr = "12345678..9";
+        var invNumStrIsNumber = invNumStr.IsNumber();
 
-            var invNumStr = "12345678..9";
-            var invNumStrIsNumber = invNumStr.IsNumber();
+        Assert.IsTrue(numStrIsNumbber);
+        Assert.IsFalse(invNumStrIsNumber);
+    }
 
-            Assert.IsTrue(numStrIsNumbber);
-            Assert.IsFalse(invNumStrIsNumber);
-        }
+    [TestMethod]
+    public void SliceTest()
+    {
+        var sliceStr = "Mohammad Mirmostafa is a C# Developer.";
 
-        [TestMethod]
-        public void SliceTest()
-        {
-            var sliceStr = "Mohammad Mirmostafa is a C# Developer.";
+        var mohammad = sliceStr.Slice(0, 8);
+        Assert.AreEqual(mohammad, "Mohammad");
 
-            var mohammad = sliceStr.Slice(0, 8);
-            Assert.AreEqual(mohammad, "Mohammad");
+        var cShartDeveloper = sliceStr.Slice(25);
+        Assert.AreEqual(cShartDeveloper, "C# Developer.");
+    }
 
-            var cShartDeveloper = sliceStr.Slice(25);
-            Assert.AreEqual(cShartDeveloper, "C# Developer.");
-        }
+    [TestMethod]
+    public void SinglarizeTest()
+    {
+        Assert.AreEqual("number", StringHelper.Singularize("numbers"));
+        Assert.AreEqual("case", StringHelper.Singularize("cases"));
+        Assert.AreEqual("handy", StringHelper.Singularize("handies"));
+        Assert.AreEqual("person", StringHelper.Singularize("people"));
+        Assert.AreEqual("child", StringHelper.Singularize("children"));
+    }
+    [TestMethod]
+    public void PluralizeTest()
+    {
+        Assert.AreEqual("numbers", StringHelper.Pluralize("number"));
+        Assert.AreEqual("cases", StringHelper.Pluralize("case"));
+        Assert.AreEqual("handies", StringHelper.Pluralize("handy"));
+        Assert.AreEqual("people", StringHelper.Pluralize("person"));
+        Assert.AreEqual("children", StringHelper.Pluralize("child"));
+    }
 
-        [TestMethod]
-        public void SinglarizeTest()
-        {
-            Assert.AreEqual("number", StringHelper.Singularize("numbers"));
-            Assert.AreEqual("case", StringHelper.Singularize("cases"));
-            Assert.AreEqual("handy", StringHelper.Singularize("handies"));
-            Assert.AreEqual("person", StringHelper.Singularize("people"));
-            Assert.AreEqual("child", StringHelper.Singularize("children"));
-        }
-        [TestMethod]
-        public void PluralizeTest()
-        {
-            Assert.AreEqual("numbers", StringHelper.Pluralize("number"));
-            Assert.AreEqual("cases", StringHelper.Pluralize("case"));
-            Assert.AreEqual("handies", StringHelper.Pluralize("handy"));
-            Assert.AreEqual("people", StringHelper.Pluralize("person"));
-            Assert.AreEqual("children", StringHelper.Pluralize("child"));
-        }
+    [TestMethod]
+    public void SpaceTest()
+        => Assert.AreEqual("     ", StringHelper.Space(5));
 
-        [TestMethod]
-        public void SpaceTest()
-            => Assert.AreEqual("     ", StringHelper.Space(5));
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void SpaceExceptionTest()
+    {
+        var expected = "     ";
+        var actual = StringHelper.Space(-5);
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void SpaceExceptionTest()
-        {
-            var expected = "     ";
-            var actual = StringHelper.Space(-5);
-            Assert.AreEqual(expected, actual);
-        }
+    [TestMethod]
+    public void GetPhraseTest()
+    {
+        var result1 = text.GetPhrase(0, '\'', '\'');
+        Assert.AreEqual("a text", result1);
 
-        [TestMethod]
-        public void GetPhraseTest()
-        {
-            var result1 = text.GetPhrase(0, '\'', '\'');
-            Assert.AreEqual("a text", result1);
+        result1 = text.GetPhrase(0, '\'');
+        Assert.AreEqual("a text", result1);
 
-            result1 = text.GetPhrase(0, '\'');
-            Assert.AreEqual("a text", result1);
+        var result2 = text.GetPhrase(1, '\'', '\'');
+        Assert.AreEqual("another text", result2);
 
-            var result2 = text.GetPhrase(1, '\'', '\'');
-            Assert.AreEqual("another text", result2);
+        var result3 = text.GetPhrase(1, 'q');
+        Assert.AreEqual(null, result3);
+    }
 
-            var result3 = text.GetPhrase(1, 'q');
-            Assert.AreEqual(null, result3);
-        }
-
-        [TestMethod]
-        public void GetPhraseTest2()
-        {
-            var result4 = text.GetPhrase(0, '\'', '\"');
-            Assert.AreEqual("a text', inside 'another text'. I want 'to find\"", result4);
-        }
+    [TestMethod]
+    public void GetPhraseTest2()
+    {
+        var result4 = text.GetPhrase(0, '\'', '\"');
+        Assert.AreEqual("a text', inside 'another text'. I want 'to find\"", result4);
     }
 }
---
+++ Tests/LibraryTest/WebResultTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using Library.Web;
+
+namespace LibraryTest;
+
+[TestClass]
+public class WebResultTest
+{
+    [TestMethod]
+    public void OkTest1()
+    {
+        var result = WebResult.Ok();
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    public void OkTest2()
+    {
+        var result = WebResult<int>.Ok(5);
+        Assert.AreEqual(5, result.Value);
+    }
+
+    [TestMethod]
+    public void OkTest3()
+    {
+        var result = WebResult.Ok(message: "Ok");
+        Assert.AreEqual("Ok", result.Message);
+    }
+
+    [TestMethod]
+    public void OkTest4()
+    {
+        var result = WebResult<int>.Ok(message: "Ok", value: 5);
+        Assert.AreEqual(5, result.Value);
+    }
+
+    [TestMethod]
+    public void FailTest1()
+    {
+        var result = WebResult.BadRequest();
+        Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [TestMethod]
+    public void ExtensionsTest1()
+    {
+        var num = -1;
+        var result = succeed()
+                        .OnSuccess(r => num = 5)
+                        .OnFailure(r => num = 6);
+        Assert.AreEqual(5, num);
+
+        static WebResult succeed() => WebResult.Ok();
+    }
+    [TestMethod]
+    public void ExtensionsTest2()
+    {
+        var num = -1;
+        var result = failed()
+                        .OnSuccess(r => num = 5)
+                        .OnFailure(r => num = 6);
+        Assert.AreEqual(6, num);
+
+        static WebResult failed() => WebResult.BadRequest();
+    }
+}
