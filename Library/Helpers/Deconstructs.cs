9e1c7d9d912efeefcbd503ba3af29fea72fe0f85
Mirmostafa <mirmostafa@hotmail.com>
2021-10-01 13:40:00 +0330

    Afew code clean up and improvements

--- Library/Helpers/Deconstructs.cs
+++ Library/Helpers/Deconstructs.cs
@@ -1,6 +1,19 @@
-﻿namespace Library.Helpers;
+﻿using System.Diagnostics.CodeAnalysis;
+using Library.Dynamic;
+
+namespace Library.Helpers;
 
 public static class Deconstructs
 {
+    //public static void Deconstruct<TKey, TValue>([DisallowNull] this Dictionary<TKey, TValue> dic, out IEnumerable<(TKey Key, TValue Value)> items)
+    //    where TKey : notnull => items = dic.ArgumentNotNull(nameof(dic)).Select(kv => (kv.Key, kv.Value));
+}
 
+public static class ExpandoHelper
+{
+    public static object? GetByPropName([DisallowNull] this Expando expando, [DisallowNull] string propName)
+        => expando.ArgumentNotNull(nameof(expando))[propName.ArgumentNotNull(nameof(propName))];
+
+    public static object? GetByPropName([DisallowNull] dynamic expando, [DisallowNull] string propName)
+        => Check.Is<Expando>(expando, nameof(expando))[propName.ArgumentNotNull(nameof(propName))];
 }
