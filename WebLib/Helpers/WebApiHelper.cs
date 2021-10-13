d737846b9b2f40d1a956e5b77db2c3f6f3727ff8
Mirmostafa <mirmostafa@hotmail.com>
2021-10-14 01:22:04 +0330

    Some improvemtns and code clean up.

---
+++ WebLib/Helpers/WebApiHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using Library.Cqrs;
+using Library.Validations;
+using Library.Web.Results;
+
+namespace Library.Web.Helpers;
+
+public static class WebApiHelper
+{
+    public static ApiResult ToApiResult(this CommandResult commandResult, string? message = null)
+        => new(commandResult.ArgumentNotNull(nameof(commandResult)).IsSucceed ? HttpStatusCode.OK : HttpStatusCode.BadRequest, message);
+
+    public static ApiResult<TResult> ToApiResult<TResult>(this CommandResult<TResult> commandResult, string? message = null)
+        => new(commandResult.ArgumentNotNull(nameof(commandResult)).IsSucceed ? HttpStatusCode.OK : HttpStatusCode.BadRequest, message, commandResult.Result);
+}
