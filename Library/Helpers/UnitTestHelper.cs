4d91bf16c3d5f108e869855b6a6f71afd9051475
Mirmostafa <mirmostafa@hotmail.com>
2021-10-20 02:10:17 +0330

    Added 'Assert.That' Extension Methods

---
+++ Library/Helpers/UnitTestHelper.cs
@@ -0,0 +1,27 @@
+﻿using Library.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Library.Helpers;
+
+public static class UnitTestHelper
+{
+    public static void IsFamiliarException(this Assert assert, Exception exception)
+        => Assert.IsInstanceOfType(exception, typeof(ExceptionBase));
+    
+    public static void ItsOk(this Assert assert) { }
+
+    public static EqualityAssertion Equal(this Assert assert, object actual) 
+        => new(actual);
+
+    //internal static void HandleFail(string assertionName, string message)
+    //    => throw new AssertFailedException($"Assertion {assertionName} failed with message: {message}");
+}
+
+public class EqualityAssertion
+{
+    private readonly object actual;
+
+    public EqualityAssertion(object actual) => this.actual = actual;
+    public void With(object expected)
+        => Assert.AreEqual(expected, this.actual);
+}
---
+++ Tests/LibraryTest/AssertHelperTest.cs
@@ -0,0 +1,13 @@
+﻿using Library.Exceptions.Validations;
+namespace LibraryTest;
+
+[TestClass]
+
+public class AssertHelperTest
+{
+    [TestMethod]
+    public void TestMethod1() => Assert.That.Equal(1).With(1);
+
+    [TestMethod]
+    public void TestMethod2() => Assert.That.IsFamiliarException(new ValidationException());
+}
