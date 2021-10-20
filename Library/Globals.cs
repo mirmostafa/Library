97ececc19cf565da479a761bc4035ce6bb314d65
Mirmostafa <mirmostafa@hotmail.com>
2021-10-20 13:58:41 +0330

    Fixed projects' assembly names

--- Library/Globals.cs
+++ Library/Globals.cs
@@ -1,14 +1,14 @@
-﻿global using System;
+﻿global using Library.Helpers;
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
-global using Library.Helpers;
 global using static Library.Coding.CodeHelper;
 global using static Library.Coding.Functional;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("WpfLib")]
-[assembly: InternalsVisibleTo("WebLib")]
+[assembly: InternalsVisibleTo("Library.Wpf")]
+[assembly: InternalsVisibleTo("Library.Web")]
 [assembly: InternalsVisibleTo("LibraryTest")]
 [assembly: InternalsVisibleTo("Library.Cqrs")]
