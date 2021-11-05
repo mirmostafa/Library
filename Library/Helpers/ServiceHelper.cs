d25a980524633a666e90451b5e5cf05d8b41a5b7
Mirmostafa <mirmostafa@hotmail.com>
2021-11-05 20:19:50 +0330

    Removed `argName`'s mandatory

--- Library/Helpers/ServiceHelper.cs
+++ Library/Helpers/ServiceHelper.cs
@@ -8,8 +8,8 @@ public static class ServiceHelper
     public static async Task<long> SaveAsync<TViewModel>(this IWriteAsyncService<TViewModel> service, TViewModel? model, bool persist = true)
         where TViewModel : IHasId<long?>
     {
-        Check.IfArgumentNotNull(service, nameof(service));
-        Check.IfArgumentNotNull(model, nameof(model));
+        Check.IfArgumentNotNull(service);
+        Check.IfArgumentNotNull(model);
         if (model.Id is { } id)
         {
             return await service.UpdateAsync(id, model, persist);
