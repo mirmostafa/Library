9b3ab1eff3085dc91d182fd12b0759d428a14e59
Mirmostafa <mirmostafa@hotmail.com>
2021-11-14 03:24:33 +0330

    Some improvements on DbContextHelper class

--- Library/Data/Markers/IHasId.cs
+++ Library/Data/Markers/IHasId.cs
@@ -1,6 +1,9 @@
-﻿namespace Library.Data.Markers;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Markers;
 
 public interface IHasId<TIdType>
 {
+    [Key]
     TIdType Id { get; set; }
 }
