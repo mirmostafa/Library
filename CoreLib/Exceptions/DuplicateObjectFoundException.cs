8b368bf6dc32317d414b141b57e6f958c62934dd
Mirmostafa <mirmostafa@hotmail.com>
2021-11-20 23:56:11 +0330

    Added DuplicateObjectFoundException

---
+++ CoreLib/Exceptions/DuplicateObjectFoundException.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Exceptions;
+
+[Serializable]
+public class DuplicateObjectFoundException : LibraryExceptionBase
+{
+    public DuplicateObjectFoundException()
+        : this("Object not found")
+    {
+    }
+
+    public DuplicateObjectFoundException(string message) : base(message)
+    {
+    }
+
+    public DuplicateObjectFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    public DuplicateObjectFoundException(string message, string? instruction = null, string? title = null, string? details = null, Exception? inner = null, object? owner = null) : base(message, instruction, title, details, inner, owner)
+    {
+    }
+}
+
