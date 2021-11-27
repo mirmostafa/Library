1b70a0e4265e95cfde8c84abb97fac4c4e8ce822
Mirmostafa <mirmostafa@hotmail.com>
2021-11-28 01:29:19 +0330

    Some improvements on Result

--- CoreLib/Results/Result.cs
+++ CoreLib/Results/Result.cs
@@ -4,29 +4,37 @@ public sealed class Result : ResultBase
 {
     public Result(int? statusCode = null, string? message = null)
         : base(statusCode, message) { }
+
+    public static Result Success => new();
+    public static Result Fail =>
+        CreateFail(-1);
+    public static Result CreateFail(int erroCode = -1, string? message = null) =>
+        new(erroCode, message);
+
+    public void Deconstruct(out int? ErrorCode, out string? Message)
+        => (ErrorCode, Message) = (this.StatusCode, this.Message);
 }
 
 public sealed class Result<TValue> : ResultBase
 {
     public TValue Value { get; }
+
     public Result(TValue value, int? statusCode = null, string? message = null)
         : base(statusCode, message) => this.Value = value;
-}
 
-public sealed record TryMethodResult<TResult>
-{
-    public TryMethodResult(bool succeed, TResult result)
-        => (this.Succeed, this.Result) = (succeed, result);
+    public static Result<TValue?> Fail =>
+        CreateFail(-1);
 
-    public bool Succeed { get; }
-    public TResult Result { get; }
+    public static Result<TValue?> CreateFail(int errorCode = -1, string? message = null, TValue? value = default!) =>
+        new(value, errorCode, message);
+    public static Result<TValue?> CreateSuccess(TValue value, int errorCode = 0, string? message = null) =>
+        new(value, errorCode, message);
 
-    public void Deconstruct(out bool succeed, out TResult result)
-        => (succeed, result) = (this.Succeed, this.Result);
+    public static implicit operator TValue(Result<TValue> result) =>
+        result.Value;
+    public static implicit operator bool(Result<TValue> result) =>
+        result?.IsSucceed ?? false;
 
-    public static implicit operator TResult(in TryMethodResult<TResult> result)
-        => result.Result;
-
-    public static implicit operator bool(in TryMethodResult<TResult> result)
-        => result.Succeed;
+    public void Deconstruct(out int? StatusCode, out string? Message, TValue Value) =>
+        (StatusCode, Message, Value) = (this.StatusCode, this.Message, this.Value);
 }
--- CoreLib/Results/ResultBase.cs
+++ CoreLib/Results/ResultBase.cs
@@ -4,22 +4,30 @@ public abstract class ResultBase : IEquatable<ResultBase?>
 {
     public int? StatusCode { get; }
     public string? Message { get; set; }
-    public virtual bool IsSucceed => this.StatusCode?.ToInt() is 0;
-    public bool Failure => !this.IsSucceed;
+    public virtual bool IsSucceed => this.StatusCode?.ToInt() is null or 0;
+    public bool IsFailure => !this.IsSucceed;
     public Dictionary<string, object> Extra { get; } = new();
-    public List<string> Errors { get; } = new();
+    public List<ErrorResult> Errors { get; } = new();
 
-    protected ResultBase(int? statusCode = null, string? message = null)
-        => (this.StatusCode, this.Message) = (statusCode, message);
+    protected ResultBase(int? statusCode = null, string? message = null) =>
+        (this.StatusCode, this.Message) = (statusCode, message);
 
-    public void Deconstruct(out int? statusCode, out string? message)
-        => (statusCode, message) = (this.StatusCode, this.Message);
-    public override bool Equals(object? obj) => this.Equals(obj as ResultBase);
-    public bool Equals(ResultBase? other) => other != null && this.StatusCode == other.StatusCode;
-    public override int GetHashCode() => HashCode.Combine(this.StatusCode);
+    public void Deconstruct(out int? statusCode, out string? message) =>
+        (statusCode, message) = (this.StatusCode, this.Message);
+    public override bool Equals(object? obj) =>
+        this.Equals(obj as ResultBase);
+    public bool Equals(ResultBase? other) =>
+        other != null && this.StatusCode == other.StatusCode;
+    public override int GetHashCode() =>
+        HashCode.Combine(this.StatusCode);
 
-    public static bool operator ==(ResultBase? left, ResultBase? right)
-        => EqualityComparer<ResultBase>.Default.Equals(left, right);
-    public static bool operator !=(ResultBase? left, ResultBase? right)
-        => !(left == right);
+    public static bool operator ==(ResultBase? left, ResultBase? right) =>
+        EqualityComparer<ResultBase>.Default.Equals(left, right);
+    public static bool operator !=(ResultBase? left, ResultBase? right) =>
+        !(left == right);
+
+    public static implicit operator bool(ResultBase result) =>
+        result?.IsSucceed ?? false;
 }
+
+public record ErrorResult(object Code, string? Message = null);
