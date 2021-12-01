07da48f35b2b8e6933d17a0f994e35ba02c85422
Mirmostafa <mirmostafa@hotmail.com>
2021-12-01 17:39:09 +0330

    Test

--- CoreLib/MultiOperation/Operation.cs
+++ CoreLib/MultiOperation/Operation.cs
@@ -6,6 +6,10 @@ public sealed class Operation<TState>
     public int? Sequence { get; internal set; }
     public string? Description { get; internal set; }
 
+
+
+
+
     public Operation(Func<(TState State, int Index, int Count), TState> action, int? sequence = null, string? description = null) =>
         (this.Action, this.Sequence, this.Description) = (action, sequence, description);
 }
