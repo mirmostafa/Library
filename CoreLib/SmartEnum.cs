db8ad11d4b8908ae724633c3f48b6c65e0fe3235
Mirmostafa <mirmostafa@hotmail.com>
2021-12-10 21:24:02 +0330

    Done. But not tested yet.

--- CoreLib/SmartEnum.cs
+++ CoreLib/SmartEnum.cs
@@ -1,5 +1,8 @@
-﻿namespace Library;
+﻿using System.Diagnostics;
 
+namespace Library;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public abstract class SmartEnum<TSmartEnum, TId> : IEquatable<TSmartEnum>
     where TSmartEnum : SmartEnum<TSmartEnum, TId>
     where TId : notnull
@@ -28,12 +31,27 @@ public abstract class SmartEnum<TSmartEnum, TId> : IEquatable<TSmartEnum>
     public override int GetHashCode() =>
         this.Id?.GetHashCode() ?? default;
 
+    public override string ToString() =>
+        this.FriendlyName ?? string.Empty;
+
     public static TSmartEnum? ById(TId id) =>
-        typeof(TSmartEnum).GetFields(System.Reflection.BindingFlags.Static)
-                          .Where(x => x.FieldType == typeof(TSmartEnum))
-                          .Select(x => x.GetValue(null).As<TSmartEnum>())
-                          .Where(x => x is not null && (x.Id?.Equals(id) ?? id is null))
-                          .SingleOrDefault();
+        GetEnumItems().Where(x => x.Id?.Equals(id) ?? id is null).SingleOrDefault();
+    public static IEnumerable<TSmartEnum> ByName(string? name) =>
+        GetEnumItems().Where(x => x.FriendlyName == name);
+
+    private static IEnumerable<TSmartEnum> GetEnumItems()
+    {
+        var type = typeof(TSmartEnum);
+        //var staticFields = type.GetFields(System.Reflection.BindingFlags.Static);
+        var staticFields = type.GetFields();
+        var smartEnums = staticFields.Where(x => x.FieldType == typeof(TSmartEnum));
+        var rawResult = smartEnums.Select(x => x.GetValue(null).As<TSmartEnum>());
+        var result = rawResult.Compact();
+        return result;
+    }
+
+    private string GetDebuggerDisplay() =>
+        this.ToString();
 }
 
 public abstract class SmartEnum<TSmartEnum> : SmartEnum<TSmartEnum, int>
--- Tests/LibraryTest/SmartEnumTest.cs
+++ Tests/LibraryTest/SmartEnumTest.cs
@@ -24,19 +24,29 @@ public class SmartEnumTest
     }
 
     [TestMethod]
-    public void EqualTest3()
-    {
-        var male1 = GenderSmartEnum.Male;
-        var male2 = GenderSmartEnum.Male;
-        Assert.IsFalse(male1 == male2);
-    }
-
-    [TestMethod]
-    public void EqualTest4()
+    public void ByIdTest()
     {
         var male1 = GenderSmartEnum.Male;
         var male2 = GenderSmartEnum.ById(male1.Id);
-        Assert.IsFalse(male1 == male2);
+        Assert.IsTrue(male1 == male2);
+    }
+
+    [TestMethod]
+    public void ByNameTest1()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var fmale = GenderSmartEnum.Female;
+        var male2 = GenderSmartEnum.ByName(male1.FriendlyName).Single();
+        Assert.IsTrue(male1 == male2);
+    }
+
+    [TestMethod]
+    public void ByNameTest2()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var fmale1 = GenderSmartEnum.Female;
+        var fmale2 = GenderSmartEnum.ByName(fmale1.FriendlyName).Single();
+        Assert.IsFalse(male1 == fmale2);
     }
 }
 
@@ -45,7 +55,7 @@ internal class GenderSmartEnum : SmartEnum<GenderSmartEnum>
 {
     public static readonly GenderSmartEnum None = new(0, "Unknown");
     public static readonly GenderSmartEnum Male = new(1, "Male");
-    public static readonly GenderSmartEnum Female = new(2, "Femail");
+    public static readonly GenderSmartEnum Female = new(2, "Female");
     public GenderSmartEnum(int id, string? friendlyName)
         : base(id, friendlyName)
     {
