e57289cf9d5fe80a1bec1e2ad24d193e046789d5
Mirmostafa <mirmostafa@hotmail.com>
2021-12-10 16:08:43 +0330

    Checkpoint: The primitive class and tests are created.

---
+++ CoreLib/SmartEnum.cs
@@ -0,0 +1,46 @@
+﻿namespace Library;
+
+public abstract class SmartEnum<TSmartEnum, TId> : IEquatable<TSmartEnum>
+    where TSmartEnum : SmartEnum<TSmartEnum, TId>
+    where TId : notnull
+{
+    public SmartEnum(TId id, string? friendlyName)
+        => (this.Id, this.FriendlyName) = (id, friendlyName);
+
+    public TId Id { get; }
+    public string? FriendlyName { get; }
+
+    public bool Equals(TSmartEnum? other) =>
+        other is not null
+        && (this.Id is null
+                ? other.Id is null
+                : this.Id.Equals(other.Id));
+
+    public static bool operator ==(SmartEnum<TSmartEnum, TId>? enum1, SmartEnum<TSmartEnum, TId>? enum2) =>
+        enum1?.Equals(enum2) ?? enum2 is null;
+
+    public static bool operator !=(SmartEnum<TSmartEnum, TId>? enum1, SmartEnum<TSmartEnum, TId>? enum2) =>
+        !(enum1 == enum2);
+
+    public override bool Equals(object? obj) =>
+        obj is SmartEnum<TSmartEnum, TId> other && this.Equals(other);
+
+    public override int GetHashCode() =>
+        this.Id?.GetHashCode() ?? default;
+
+    public static TSmartEnum? ById(TId id) =>
+        typeof(TSmartEnum).GetFields(System.Reflection.BindingFlags.Static)
+                          .Where(x => x.FieldType == typeof(TSmartEnum))
+                          .Select(x => x.GetValue(null).As<TSmartEnum>())
+                          .Where(x => x is not null && (x.Id?.Equals(id) ?? id is null))
+                          .SingleOrDefault();
+}
+
+public abstract class SmartEnum<TSmartEnum> : SmartEnum<TSmartEnum, int>
+    where TSmartEnum : SmartEnum<TSmartEnum>
+{
+    protected SmartEnum(int id, string? friendlyName)
+        : base(id, friendlyName)
+    {
+    }
+}
---
+++ Tests/LibraryTest/SmartEnumTest.cs
@@ -0,0 +1,53 @@
+﻿using Library;
+
+namespace LibraryTest;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2252:This API requires opting into preview features", Justification = "<Pending>")]
+[TestClass]
+public class SmartEnumTest
+{
+
+    [TestMethod]
+    public void EqualTest1()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var male2 = GenderSmartEnum.Male;
+        Assert.AreEqual(male1, male2);
+    }
+
+    [TestMethod]
+    public void EqualTest2()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var male2 = GenderSmartEnum.Male;
+        Assert.IsTrue(male1 == male2);
+    }
+
+    [TestMethod]
+    public void EqualTest3()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var male2 = GenderSmartEnum.Male;
+        Assert.IsFalse(male1 == male2);
+    }
+
+    [TestMethod]
+    public void EqualTest4()
+    {
+        var male1 = GenderSmartEnum.Male;
+        var male2 = GenderSmartEnum.ById(male1.Id);
+        Assert.IsFalse(male1 == male2);
+    }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2252:This API requires opting into preview features", Justification = "<Pending>")]
+internal class GenderSmartEnum : SmartEnum<GenderSmartEnum>
+{
+    public static readonly GenderSmartEnum None = new(0, "Unknown");
+    public static readonly GenderSmartEnum Male = new(1, "Male");
+    public static readonly GenderSmartEnum Female = new(2, "Femail");
+    public GenderSmartEnum(int id, string? friendlyName)
+        : base(id, friendlyName)
+    {
+    }
+}
