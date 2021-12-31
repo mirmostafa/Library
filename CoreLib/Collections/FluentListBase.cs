32c6425c9aafd3335390d3b2f1e6533c2ccbedc5
Mirmostafa <mirmostafa@hotmail.com>
2021-12-31 22:41:09 +0330

    Added ObservableColletionWrapper.

--- CoreLib/Collections/FluentListBase.cs
+++ CoreLib/Collections/FluentListBase.cs
@@ -10,21 +10,26 @@ namespace Library.Collections;
 public class FluentListBase<TItem, TList> : IFluentList<TList, TItem>//, IImmutableList<TItem>
     where TList : FluentListBase<TItem, TList>
 {
-    private readonly List<TItem> _List;
+    private readonly IList<TItem> _list;
+    private bool _isReadOnly;
 
-    public bool IsReadOnly { get; protected set; }
+    public bool IsReadOnly
+    {
+        get => this._isReadOnly || this._list.IsReadOnly;
+        protected set => this._isReadOnly = value;
+    }
 
     public int Count =>
-        this._List.Count;
+        this._list.Count;
 
     bool IFluentCollection<TList, TItem>.IsReadOnly { get; }
 
     public TItem this[int index]
     {
-        get => this._List[index]; set
+        get => this._list[index]; set
         {
             this.CheckReadOnly();
-            this._List[index] = value;
+            this._list[index] = value;
         }
     }
 
@@ -32,39 +37,39 @@ public class FluentListBase<TItem, TList> : IFluentList<TList, TItem>//, IImmuta
     protected TList CheckReadOnly() =>
        !this.IsReadOnly ? this.This : throw new Exceptions.InvalidOperationException("This list is marked as [Read Only].");
 
-    protected FluentListBase(List<TItem> list) =>
-        this._List = list;
+    protected FluentListBase(IList<TItem> list) =>
+        this._list = list ?? new List<TItem>();
     protected FluentListBase(IEnumerable<TItem> list) =>
-        this._List = new(list);
+        this._list = new List<TItem>(list);
     protected FluentListBase(int capacity) =>
-        this._List = new(capacity);
+        this._list = new List<TItem>(capacity);
     protected FluentListBase() =>
-        this._List = new();
+        this._list = new List<TItem>();
 
     public (TList List, int Result) IndexOf(TItem item) =>
-        (this.This, this._List.IndexOf(item));
+        (this.This, this._list.IndexOf(item));
     public TList Insert(int index, TItem item) =>
-        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._List.Insert(index, item));
+        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._list.Insert(index, item));
     public TList RemoveAt(int index) =>
-        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._List.RemoveAt(index));
+        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._list.RemoveAt(index));
     public TList Add(TItem item) =>
-        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._List.Add(item));
+        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._list.Add(item));
     public TList Clear() =>
-        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._List.Clear());
+        this.Fluent(this.CheckReadOnly).This.Fluent(() => this._list.Clear());
     public (TList List, bool Result) Contains(TItem item) =>
-        (this.This, this._List.Contains(item));
+        (this.This, this._list.Contains(item));
     public TList CopyTo(TItem[] array, int arrayIndex) =>
-        this.This.Fluent(() => this._List.CopyTo(array, arrayIndex));
+        this.This.Fluent(() => this._list.CopyTo(array, arrayIndex));
     public (TList List, bool Result) Remove(TItem item) =>
-        this.Fluent(this.CheckReadOnly).This.FluentByResult(() => this._List.Remove(item));
+        this.Fluent(this.CheckReadOnly).This.FluentByResult(() => this._list.Remove(item));
 
     public IEnumerator<TItem> GetEnumerator() =>
-        this._List.GetEnumerator();
+        this._list.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() =>
-        ((IEnumerable)this._List).GetEnumerator();
+        ((IEnumerable)this._list).GetEnumerator();
 
     public List<TItem> AsList() =>
-        this._List.ToList();
+        this._list.ToList();
 
     private TList This =>
         this.As<TList>()!;
