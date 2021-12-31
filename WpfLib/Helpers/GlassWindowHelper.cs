34b9a86c97b23d8f1d091631836e0a2b599f2507
Mirmostafa <mirmostafa@hotmail.com>
2021-12-31 20:56:31 +0330

    Added glass window support

---
+++ WpfLib/Helpers/GlassWindowHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace Library.Wpf.Helpers;
+
+public static class WindowHelper
+{
+    // this code is taken from a sample application provided by Rafael Rivera
+    // see the full code sample here: (2016/08)
+    // https://github.com/riverar/sample-win10-aeroglass
+    public static void EnableBlur(Window win, bool enable = true)
+    {
+        var windowHelper = new WindowInteropHelper(win);
+
+        var accent = new AccentPolicy
+        {
+            AccentState = enable ? AccentState.AccentEnableBlurbehind : AccentState.AccentDisabled
+        };
+
+        var accentStructSize = Marshal.SizeOf(accent);
+
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+
+        var data = new WindowCompositionAttributeData
+        {
+            Attribute = WindowCompositionAttribute.WcaAccentPolicy,
+            SizeOfData = accentStructSize,
+            Data = accentPtr
+        };
+
+        SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+        Marshal.FreeHGlobal(accentPtr);
+    }
+    
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+    internal enum AccentState
+    {
+        AccentDisabled = 0,
+        AccentEnableGradient = 1,
+        AccentEnableTransparentgradient = 2,
+        AccentEnableBlurbehind = 3,
+        AccentInvalidState = 4
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+    internal enum WindowCompositionAttribute
+    {
+        // ...
+        WcaAccentPolicy = 19
+        // ...
+    }
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+}
