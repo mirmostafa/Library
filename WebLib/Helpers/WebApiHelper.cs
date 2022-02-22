e3ebaa1d51603b3c526ab1068a29e3724995dc1a
Mirmostafa <mirmostafa@hotmail.com>
2022-02-22 05:02:08 +0330

    Initially done. That's enough for now.

--- WebLib/Helpers/WebApiHelper.cs
+++ WebLib/Helpers/WebApiHelper.cs
@@ -2,8 +2,9 @@
 using Library.Cqrs;
 using Library.Validations;
 using Library.Web.Results;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Library.Web.Helpers;
+namespace Library.Helpers;
 
 public static class WebApiHelper
 {
@@ -12,4 +13,12 @@ public static class WebApiHelper
 
     public static ApiResult<TResult> ToApiResult<TResult>(this CommandResult<TResult> commandResult, string? message = null)
         => new(commandResult.ArgumentNotNull(nameof(commandResult)).IsSucceed ? HttpStatusCode.OK : HttpStatusCode.BadRequest, message, commandResult.Result);
+
+    public static ContentResult ToHtml(string content)
+            => new()
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = content
+            };
 }
---
+++ WebLib/WebApiCreator.cs
@@ -0,0 +1,68 @@
+﻿using Library.Coding;
+using Library.Helpers;
+
+namespace Library.Web;
+public class WebApiCreator
+{
+    public WebApiCreator(WebApplication? app = null, IEnumerable<string>? args = null) =>
+        this.App = GetApp(app) ?? WebApplication.CreateBuilder(args?.ToArray() ?? Array.Empty<string>()).Build();
+
+    public static WebApiCreator New(WebApplication? app = null, params string[] urls) =>
+        new WebApiCreator(app).AddUrl(urls);
+    public static WebApiCreator New(params string[] urls) =>
+        new WebApiCreator(null).AddUrl(urls);
+
+    public WebApplication App { get; }
+    public ICollection<string> Urls => this.App.Urls;
+
+    public static (WebApiCreator Creator, WebApplication App) Create(in WebApplication? app, in HttpMethod method, in string route, in Delegate body)
+    {
+        var creator = New(app).Create(method, route, body);
+        return (creator, creator.App);
+    }
+
+    private static WebApplication GetApp(WebApplication? app) =>
+        app ?? WebApplication.CreateBuilder().Build();
+
+    public WebApiCreator Create(in HttpMethod method, in string route!!, in Delegate body!!)
+    {
+        _ = method switch
+        {
+            HttpMethod.None => null,
+            HttpMethod.Get => this.App.MapGet(route, body),
+            HttpMethod.Post => this.App.MapPost(route, body),
+            HttpMethod.Put => this.App.MapPut(route, body),
+            HttpMethod.Patch => throw new NotSupportedException(),
+            HttpMethod.Delete => this.App.MapDelete(route, body),
+            _ => throw new NotSupportedException(),
+        };
+        return this;
+    }
+
+    public WebApiCreator MapDelete(string route, Delegate body) =>
+        this.Create(HttpMethod.Delete, route, body);
+    public WebApiCreator MapPut(string route, Delegate body) =>
+        this.Create(HttpMethod.Put, route, body);
+    public WebApiCreator MapPost(string route, Delegate body) =>
+        this.Create(HttpMethod.Post, route, body);
+    public WebApiCreator MapGet(string route, Delegate body) =>
+        this.Create(HttpMethod.Get, route, body);
+
+    public void Deconstruct(out WebApiCreator creator, out WebApplication app) =>
+        (creator, app) = (this, this.App);
+
+    public WebApiCreator AddUrl(params string[] url) =>
+        this.Fluent(() => this.Urls.AddRange(url));
+    public Task<WebApiCreator> RunAsync() =>
+        this.FluentAsync(() => this.App.RunAsync());
+}
+
+public enum HttpMethod
+{
+    None = 0,
+    Get = 1,
+    Post = 2,
+    Put = 3,
+    Patch = 4,
+    Delete = 5,
+}
