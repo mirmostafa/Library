c4a9a877e2feb46bd607a2bad31cbe87aff4628e
Mirmostafa <mirmostafa@hotmail.com>
2022-03-05 14:17:39 +0330

    Just a few code clean-up on `TestConApp`

---
+++ Tests/TestConApp/ConsoleAppStartupModule.cs
@@ -0,0 +1,18 @@
+﻿using Library.Logging;
+using System.Runtime.CompilerServices;
+
+namespace ConAppTest;
+
+public static class ConsoleAppStartupModule
+{
+    public static ILogger Logger { get; private set; } = null!;
+
+    [ModuleInitializer]
+    public static void Startup()
+    {
+        Loggers loggers = new();
+        loggers.Add(new VsOutputLogger());
+        loggers.Add(new TextWriterLogger(Console.Out));
+        Logger = loggers;
+    }
+}
