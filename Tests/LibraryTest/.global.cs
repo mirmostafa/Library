81b9b1dd6f2ba2c1496ed226de659d21229b8b9e
Mirmostafa <mirmostafa@hotmail.com>
2022-03-28 09:53:47 +0430

    Solving CI/CD Compile Error

---
+++ Tests/LibraryTest/.global.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Library.Helpers;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using static Library.Coding.Functional;
--- WpfLib/.globals.cs
+++ WpfLib/.globals.cs
@@ -5,7 +5,10 @@ global using System.Text;
 global using System.Threading.Tasks;
 global using System.Windows;
 global using System.Windows.Controls;
+global using Library.Coding;
 global using Library.Helpers;
+global using Library.Logging;
+global using Library.Validations;
 global using Library.Wpf.Helpers;
-
+global using Library.Wpf.Media;
 global using static Library.Coding.Functional;
