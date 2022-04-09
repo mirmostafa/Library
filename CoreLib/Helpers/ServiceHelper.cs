b4ef04c5bfbdee7aca7252d88a5e6672563f3a34
Mirmostafa <mirmostafa@hotmail.com>
2022-04-09 19:29:08 +0430

    Some improvements on `ServiceHelper` class

--- CoreLib/Helpers/ServiceHelper.cs
+++ CoreLib/Helpers/ServiceHelper.cs
@@ -7,40 +7,25 @@ using Microsoft.EntityFrameworkCore.Storage;
 namespace Library.Helpers;
 public static class ServiceHelper
 {
-    public static async Task<TViewModel> SubmitChangesAsync<TViewModel>(this IAsyncWriteService<TViewModel> service, TViewModel? model, bool persist = true)
+    public static async Task<TViewModel> SaveViewModelAsync<TViewModel>(this IAsyncWriteService<TViewModel> service!!, TViewModel model, bool persist = true)
         where TViewModel : ICanSetKey<long?>
-    {
-        Check.IfArgumentNotNull(service);
-        Check.IfArgumentNotNull(model);
-
-        return model.Id is { } id
+        => model.ArgumentNotNull().Id is { } id and > 0
             ? await service.UpdateAsync(id, model, persist)
             : await service.InsertAsync(model, persist);
-    }
 
-    public static async Task<TViewModel> SumbitChangesAsync<TViewModel>(this IAsyncWriteService<TViewModel, Guid> service, TViewModel model, bool persist = true)
+    public static async Task<TViewModel> SaveViewModelAsync<TViewModel>(this IAsyncWriteService<TViewModel, Guid> service!!, TViewModel model, bool persist = true)
         where TViewModel : ICanSetKey<Guid>
-    {
-        Check.IfArgumentNotNull(service);
-        Check.IfArgumentNotNull(model);
-
-        return !model.Id.IsNullOrEmpty()
+        => !model.ArgumentNotNull().Id.IsNullOrEmpty()
             ? await service.UpdateAsync(model.Id, model, persist)
             : await service.InsertAsync(model, persist);
-    }
 
-    public static async Task<TViewModel> SubmitChangesAsync<TViewModel>(this IAsyncWriteService<TViewModel, Id> service, TViewModel? model, bool persist = true)
+    public static async Task<TViewModel> SaveViewModelAsync<TViewModel>(this IAsyncWriteService<TViewModel, Id> service!!, TViewModel model, bool persist = true)
         where TViewModel : ICanSetKey<Id>
-    {
-        Check.IfArgumentNotNull(service);
-        Check.IfArgumentNotNull(model);
-
-        return !model.Id.IsNullOrEmpty()
+        => !model.ArgumentNotNull().Id.IsNullOrEmpty()
             ? await service.UpdateAsync(model.Id, model, persist)
             : await service.InsertAsync(model, persist);
-    }
 
-    public static async Task<int> CommitChangesAsync<TService>(this TService service, bool persist = true, IDbContextTransaction? transaction = null)
+    public static async Task<int> SubmitChangesAsync<TService>(this TService service, bool persist = true, IDbContextTransaction? transaction = null)
         where TService : IAsyncSaveService, IResetChanges
     {
         Check.IfArgumentNotNull(service);
