7d723c2b4cf25bd7c0db26742814070e64fe9576
Mirmostafa <mirmostafa@hotmail.com>
2022-04-13 22:29:59 +0430

    Some improvements

--- CoreLib/Validations/Check.cs
+++ CoreLib/Validations/Check.cs
@@ -23,6 +23,7 @@ public sealed class Check
     /// </value>
     public static Check That => _that ??= new();
 
+    [StackTraceHidden]
     public static void IfArgumentBiggerThan(in int arg, in int min, [CallerArgumentExpression("arg")] in string argName = null)
     {
         if (arg <= min)
@@ -31,9 +32,11 @@ public sealed class Check
         }
     }
 
+    [StackTraceHidden]
     public static void IfIs<T>([NotNull] object obj, [CallerArgumentExpression("obj")] string argName = null) =>
         _ = obj.NotValid(x => x is not T, () => new TypeMismatchValidationException(argName!));
 
+    [StackTraceHidden]
     [return: NotNull]
     /// <summary>
     /// Makes sure the specified argument is not null.
@@ -52,12 +55,15 @@ public sealed class Check
     /// <param name="obj"></param>
     /// <param name="argName"></param>
     /// <exception cref="ArgumentNullException"/>
+    [StackTraceHidden]
     public static void IfArgumentNotNull([NotNull] object obj, [CallerArgumentExpression("obj")] string argName = null) =>
         MustBeArgumentNotNull(obj is not null, argName!);
 
+    [StackTraceHidden]
     public static void IfNotNull<T>([NotNull] T obj, [DisallowNull] Func<Exception> getException) =>
         _ = obj.NotValid(x => x is null, getException);
 
+    [StackTraceHidden]
     public static void IfHasAny([NotNull] IEnumerable obj, [DisallowNull] Func<Exception> getException)
     {
         if (!obj.NotNull(getException).Any())
@@ -65,23 +71,30 @@ public sealed class Check
             Throw(getException);
         }
     }
+    [StackTraceHidden]
     public static void IfHasAny<TEnumerable>([NotNull] IEnumerable obj, [DisallowNull] Func<Exception> getException) =>
         _ = obj.HasAny(getException);
+    [StackTraceHidden]
     public static void IfNotNull<T>([NotNull] T obj, [CallerArgumentExpression("obj")] string argName = null) =>
         MustBeNotNull(obj is not null, argName!);
 
+    [StackTraceHidden]
     public static void IfNotValid<T>(T obj, [DisallowNull] in Func<T, bool> validate, in Func<Exception> getException) =>
         obj.NotValid(validate, getException);
 
+    [StackTraceHidden]
     public static void IfAny([NotNull] IEnumerable obj, [CallerArgumentExpression("obj")] string argName = null) =>
         IfNotValid(obj, _ => !obj?.Any() ?? false, () => new NoItemValidationException(argName!));
 
+    [StackTraceHidden]
     public static void IfNotNull<T>([NotNull] T obj, Func<string> getMessage) =>
         obj.NotValid(x => x is null, () => new ValidationException(getMessage()));
 
+    [StackTraceHidden]
     public static void IfNotNull<T>([NotNull] T @this, string obj, [CallerArgumentExpression("obj")] string argName = null) =>
         _ = @this.NotValid(_ => obj.IsNullOrEmpty(), () => new NullValueValidationException(argName!))!;
 
+    [StackTraceHidden]
     public static void MustBe([DoesNotReturnIf(false)] bool ok, in Func<Exception> getExceptionIfNot)
     {
         if (!ok)
@@ -89,18 +102,24 @@ public sealed class Check
             Throw(getExceptionIfNot);
         }
     }
+    [StackTraceHidden]
     public static void MustBe<TValidationException>([DoesNotReturnIf(false)] bool ok)
         where TValidationException : Exception, new() =>
         MustBe(ok, () => new TValidationException());
+    [StackTraceHidden]
     public static void MustBe([DoesNotReturnIf(false)] bool required) =>
         MustBe<RequiredValidationException>(required);
+    [StackTraceHidden]
     public static void MustBe([DoesNotReturnIf(false)] bool required, Func<string> getMessage!!) =>
         MustBe(required, () => new ValidationException(getMessage()));
+    [StackTraceHidden]
     public static void MustBeNotNull([DoesNotReturnIf(false)] bool isNotNull, string argName) =>
         MustBe(isNotNull, () => new NullValueValidationException(argName));
+    [StackTraceHidden]
     public static void MustBeArgumentNotNull([DoesNotReturnIf(false)] bool isNotNull, [DisallowNull] string argName) =>
         MustBe(isNotNull, () => new ArgumentNullException(argName));
 
+    [StackTraceHidden]
     [return: NotNull]
     public static async Task<T> IfNotNullAsync<T>([DisallowNull] Func<Task<T>> get, [CallerArgumentExpression("get")] string message = null) =>
         (await get()).NotNull(() => new NullValueValidationException(message, null));
@@ -111,17 +130,21 @@ public sealed class Check
 public static class CheckHelpers
 {
 
+    [StackTraceHidden]
     [return: NotNull]
     public static string ArgumentNotNull([NotNull] this string obj, [CallerArgumentExpression("obj")] string argName = null) =>
         obj.NotValid(x => x.IsNullOrEmpty(), () => new ArgumentNullException(argName));
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T ArgumentNotNull<T>([NotNull] this T obj, [CallerArgumentExpression("obj")] string argName = null) =>
         obj.NotValid(x => x is null, () => new ArgumentNullException(argName));
 
+    [StackTraceHidden]
     public static T Is<T>([NotNull] this object obj, [CallerArgumentExpression("obj")] string argName = null) =>
             obj.NotValid(x => x is not T, () => new TypeMismatchValidationException(argName!)).To<T>();
 
+    [StackTraceHidden]
     public static TEnumerable HasAny<TEnumerable>([NotNull] this TEnumerable obj, [DisallowNull] Func<Exception> getException)
         where TEnumerable : IEnumerable
     {
@@ -132,45 +155,56 @@ public static class CheckHelpers
         return obj;
     }
 
+    [StackTraceHidden]
     [return: NotNull]
     public static string NotNull([NotNull] this string obj, [CallerArgumentExpression("obj")] string argName = null) =>
             obj ?? throw new NullValueValidationException(argName ?? "obj");
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T NotNull<T>([NotNull] this T obj, [DisallowNull] Func<Exception> getException) =>
         obj.NotValid(x => x is null, getException)!;
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T NotNull<T, TException>([NotNull] this T obj)
         where TException : Exception, new() =>
         obj.NotValid(x => x is null, () => new TException());
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T NotNull<T>([NotNull] this T obj, [CallerArgumentExpression("obj")] string argName = null) =>
         obj.NotValid(x => x is null, () => new NullValueValidationException(argName!))!;
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T NotNull<T>([NotNull] this T @this, [DisallowNull] object obj, [CallerArgumentExpression("obj")] string argName = null) =>
         @this.NotValid(_ => obj is null, () => new NullValueValidationException(argName!))!;
 
+    [StackTraceHidden]
     [return: NotNull]
     public static T NotNull<T>([NotNull] this T @this, string obj, [CallerArgumentExpression("obj")] string argName = null) =>
         @this.NotValid(_ => obj.IsNullOrEmpty(), () => new NullValueValidationException(argName!))!;
 
+    [StackTraceHidden]
     public static T NotValid<T>(this T obj, [DisallowNull] in Func<T, bool> validate, [DisallowNull] in Func<Exception> getException) =>
         !(validate?.Invoke(obj) ?? false) ? obj : getException is null ? obj : Throw<T>(getException);
 
+    [StackTraceHidden]
     public static void OnFalse([DoesNotReturnIf(true)] this bool ok, Func<Exception> getException)
         => Check.MustBe(ok, getException);
 
+    [StackTraceHidden]
     [return: NotNull]
     public static async Task<T> NotNullAsync<T>([DisallowNull] this Task<T> task, [CallerArgumentExpression("task")] string message = null) =>
         (await task).NotNull(() => new NullValueValidationException(message, null));
 
+    [StackTraceHidden]
     [return: NotNull]
     public static async Task<T> NotNullAsync<T>([DisallowNull] this Task<T?> task, [DisallowNull] Func<Exception> getException) =>
         (await task).NotNull(getException);
 
+    [StackTraceHidden]
     public static T ThrowIfDisposed<T>(this T @this, [DoesNotReturnIf(true)] bool disposed)
         where T : IDisposable
     {
--- WebLib/Bases/MiddlewareBase.cs
+++ WebLib/Bases/MiddlewareBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using Library.Coding;
 using Library.EventsArgs;
 
 namespace Library.Web.Bases;
@@ -7,24 +8,20 @@ public abstract class MiddlewareBase
 {
     private readonly RequestDelegate _next;
 
-    protected MiddlewareBase(RequestDelegate next) =>
-            this._next = next;
+    protected MiddlewareBase(RequestDelegate next)
+        => this._next = next;
+
     protected ClaimsPrincipal? User { get; private set; }
 
     [System.Diagnostics.DebuggerStepThrough]
     public async Task Invoke(HttpContext httpContext)
     {
-        try
-        {
-            await initialize(httpContext);
-        }
-        catch
-        {
-        }
-        await handleExecuting(httpContext);
-        await handleExecuted(httpContext);
+        CodeHelpers.CatchResult(async () => await initialize(httpContext));
+        await executing(httpContext);
+        await executed(httpContext);
+        return;
 
-        async Task handleExecuting(HttpContext httpContext)
+        async Task executing(HttpContext httpContext)
         {
             var onExecutingEventArgs = new ItemActingEventArgs<HttpContext?>(httpContext);
             await this.OnExecutingAsync(onExecutingEventArgs);
@@ -34,7 +31,7 @@ public abstract class MiddlewareBase
             }
         }
 
-        async Task handleExecuted(HttpContext httpContext)
+        async Task executed(HttpContext httpContext)
         {
             var onExecutedEventArgs = new ItemActedEventArgs<HttpContext?>(httpContext);
             await this.OnExecutedAsync(onExecutedEventArgs);
@@ -47,11 +44,11 @@ public abstract class MiddlewareBase
         }
     }
 
-    protected virtual async Task OnInitializeAsync(HttpContext? httpContext) =>
-        await Task.CompletedTask;
+    protected virtual async Task OnInitializeAsync(HttpContext? httpContext)
+        => await Task.CompletedTask;
 
-    protected virtual async Task OnExecutedAsync(ItemActedEventArgs<HttpContext?> e) =>
-        await Task.CompletedTask;
+    protected virtual async Task OnExecutedAsync(ItemActedEventArgs<HttpContext?> e)
+        => await Task.CompletedTask;
 
     protected abstract Task OnExecutingAsync(ItemActingEventArgs<HttpContext?> e);
 }
