184c01f6728d598341e19edc713fa81ff5aa54a3
Mirmostafa <mirmostafa@hotmail.com>
2022-04-24 23:05:14 +0430

    Add Enhanced `ICommand` Design Pattern to WPF Project #226 Done.

---
+++ WpfLib/DesignPatterns/Command/IControlCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Library.Wpf.DesignPatterns.Command;
+
+public interface IControlCommand : ICommand
+{
+    string Text { get; }
+    string CommandGroupName { get; }
+    bool IsEnabled { get; }
+    Visibility Visibility { get; }
+}
+
+public interface IControlCommandGroup : IControlCommand
+{
+    event EventHandler Invoke;
+
+    IList<IControlCommand> Commands { get; }
+    InputGestureCollection InputGestures { get; }
+    string Name { get; }
+}
