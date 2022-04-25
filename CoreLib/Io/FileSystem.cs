71a39a8c255ce594b9dc98ef749ae1513cc0ed88
Mirmostafa <mirmostafa@hotmail.com>
2022-04-25 23:31:57 +0430

    Enrich Library.IO namespace #224 Cont.

--- CoreLib/Io/FileSystem.cs
+++ CoreLib/Io/FileSystem.cs
@@ -1,22 +1,56 @@
 ﻿using Library.DesignPatterns.Markers;
+using Library.Validations;
+using SysDriveInfo = System.IO.DriveInfo;
 
 namespace Library.Io;
 
 [Immutable]
-public abstract class FileSystem
+public abstract class FileSystem : IEquatable<FileSystem>
 {
     public FileSystem(string fullPath!!)
         => this.FullPath = fullPath;
 
     public string FullPath { get; }
+
+    public bool Equals(FileSystem? other)
+        => other is not null && other.FullPath.Equals(this.FullPath);
+
+    public override bool Equals(object? obj)
+        => obj is FileSystem fs && fs.Equals(this);
+
+    public override int GetHashCode()
+        => this.FullPath.GetHashCode();
+
+    public static implicit operator string([DisallowNull] FileSystem fileSystem)
+        => fileSystem.ArgumentNotNull().FullPath;
 }
+
 [Immutable]
 public sealed class Drive : FileSystem
 {
-    public Drive(char driveLetter) : base($"""{driveLetter}:\""")
+    public Drive(char driveLetter)
+        : base($"""{driveLetter}:\""")
     {
     }
+
+    public static explicit operator Drive?(SysDriveInfo? sysDrive)
+        => sysDrive is null ? null : new(sysDrive.RootDirectory.FullName[0]);
+    public static explicit operator SysDriveInfo?(Drive? drive)
+        => drive is null ? null : new(drive.FullPath[0].ToString());
+
+    [return: NotNull]
+    public static IEnumerable<Drive> GetAll()
+        => SysDriveInfo.GetDrives().Select(FromSystemDrive);
+
+    [return: NotNull]
+    public static Drive FromSystemDrive([DisallowNull] SysDriveInfo sysDrive)
+        => new(sysDrive.ArgumentNotNull().RootDirectory.FullName[0]);
+
+    [return: NotNull]
+    public SysDriveInfo ToSystemDrive()
+        => new(this.FullPath[0].ToString());
 }
+
 [Immutable]
 public sealed class Folder : FileSystem
 {
@@ -24,6 +58,7 @@ public sealed class Folder : FileSystem
     {
     }
 }
+
 [Immutable]
 public sealed class File : FileSystem
 {
@@ -31,6 +66,7 @@ public sealed class File : FileSystem
     {
     }
 }
+
 public static class FileSystemHelper
 {
     public static string? Name(this FileSystem item)
