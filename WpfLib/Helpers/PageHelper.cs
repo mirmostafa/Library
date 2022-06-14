cdd5861dab4672801264b44b35077475f144fe70
Mirmostafa <mirmostafa@hotmail.com>
2022-06-14 23:13:35 +0430

    Added `AskToSaveAsync` to `PageHelper`

---
+++ WpfLib/Helpers/PageHelper.cs
@@ -0,0 +1,28 @@
+﻿using Library.Results;
+using Library.Wpf.Dialogs;
+using Library.Wpf.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Library.Wpf.Helpers;
+
+public static class PageHelper
+{
+    public static async Task<Result> AskToSaveAsync<TPage>(this TPage page)
+        where TPage : IStatefulPage, ISaveChangesAsync
+    {
+        if (page.IsViewModelChanged)
+        {
+            var askToSave = MsgBox2.AskWithCancel("Do you want to save changes?");
+            if (askToSave is TaskDialogResult.Cancel or TaskDialogResult.Close)
+            {
+                return Result.Fail;
+            }
+
+            if (askToSave is TaskDialogResult.Yes)
+            {
+                return await page.SaveChangesAsync();
+            }
+        }
+        return Result.Success;
+    }
+}
---
+++ WpfLib/Windows/IStatefullPage.cs
@@ -0,0 +1,12 @@
+﻿using Library.Results;
+
+namespace Library.Wpf.Windows;
+public interface IStatefulPage
+{
+    bool IsViewModelChanged { get; }
+}
+
+public interface ISaveChangesAsync
+{
+    Task<Result> SaveChangesAsync();
+}
