431ac182f387b31d53c14e4f0df48063822d7c39
Mirmostafa <mirmostafa@hotmail.com>
2022-06-14 23:13:00 +0430

    Improved NotifyPropertyChangedHelper

---
+++ CoreLib/Helpers/NotifyPropertyChangedHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Helpers;
+public static class NotifyPropertyChangedHelper
+{
+    public static TNotifyPropertyChanged HandlePropertyChanges<TNotifyPropertyChanged>(this TNotifyPropertyChanged o, in PropertyChangedEventHandler? propertyChangedHandler)
+        where TNotifyPropertyChanged: INotifyPropertyChanged
+    {
+        o.PropertyChanged -= propertyChangedHandler;
+        o.PropertyChanged += propertyChangedHandler;
+        return o;
+    }
+}
