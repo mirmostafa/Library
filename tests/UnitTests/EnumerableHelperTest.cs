c7ac7f954b50db9e50ad1d40ad5c2e57ecec3b40
MIRMOSTAFA-PC\Mirmostafa <mirmostafa@hotmail.com>
2023-02-06 05:02:41 +0330

    #287 Wrote some unit tests for StringHelper

--- tests/UnitTests/EnumerableHelperTest.cs
+++ tests/UnitTests/EnumerableHelperTest.cs
@@ -7,16 +7,6 @@ public class EnumerableHelperTest
     private readonly string[] _names = new[] { "Nick", "Mike", "John", "Leyla", "David", "Damian" };
     private readonly int[] _age = new[] { 10, 20, 30, 40, 50, 60 };
 
-    //[Fact]
-    //public void AddRangeAsyncTest()
-    //{
-    //    var numberQuery = Enumerable.Range(1, 10).AsQueryable();
-    //    var numbers = numberQuery.ToEnumerableAsync();
-    //    var numberList = numbers.ToListAsync().Result;
-    //    var result = numberList.AddRangeAsync(numbers).Result;
-    //    Assert.Equal(10, result.Count);
-    //}
-
     [Fact( Skip ="Not required in .Net 6.0")]
     public void ChunkByTest()
     {
