c3cb3cb523f56de37c7a3232072722a2d360af73
MIRMOSTAFA-PC\Mirmostafa <mirmostafa@hotmail.com>
2023-04-12 20:42:50 +0330

    Simplified `Proress` Added `ProgressRoutine`

---
+++ src/CoreLib/Coding/ProgressRoutine.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Coding;
+
+public sealed class ProgressRoutine<T> : IProgress<(T State, int Current, int Maximum)>
+{
+    private readonly Action<(T State, int Current, int Maximum)> _reporter;
+
+    public ProgressRoutine(Action<(T State, int Current, int Maximum)> reporter)
+        => this._reporter = reporter;
+    
+    public void Report((T State, int Current, int Maximum) value) 
+        => this._reporter(value);
+
+    public void Report(T state, int current, int maximum)
+        => this._reporter((state, current, maximum));
+}
