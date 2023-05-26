415620c78e0dc004e8e3f3bda9fcc204867f4e97
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-05-27 01:38:37 +0330

    `DI` is fully documeneted.

--- src/CoreLib/Helpers/DI.cs
+++ src/CoreLib/Helpers/DI.cs
@@ -7,10 +7,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Library.Helpers;
 
+/// <summary>
+/// DI static class provides a way to get services from the service provider.
+/// </summary>
 public static class DI
 {
     private static IServiceProvider? _serviceProvider = null;
 
+    /// <summary>
+    /// Gets the service of the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the service.</typeparam>
+    /// <returns>The service of the specified type.</returns>
+    /// <exception cref="LibraryException">Thrown when DI is not initiated.</exception>
+    /// <exception cref="ObjectNotFoundException">Thrown when unable to resolve service for the specified type.</exception>
     [DebuggerStepThrough]
     public static T GetService<T>()
     {
@@ -21,6 +31,10 @@ public static class DI
         return result;
     }
 
+    /// <summary>
+    /// Initializes the DI class.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider.</param>
     public static void Initialize(in IServiceProvider serviceProvider)
-        => _serviceProvider = serviceProvider;
+            => _serviceProvider = serviceProvider;
 }
