cc773538a95c2363e8f3ab2cd5f354a44a3ca2fe
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-05-27 01:22:32 +0330

    `BooleanHelper` is fully documented.

--- src/CoreLib/Helpers/BooleanHelper.cs
+++ src/CoreLib/Helpers/BooleanHelper.cs
@@ -2,8 +2,16 @@
 
 public static class BooleanHelper
 {
+    /// <summary>
+    /// Checks if any of the boolean values in the given array is true.
+    /// </summary>
     public static bool Any(params bool[] bools)
-        => bools.Any();
+            => bools.Any();
+    /// <summary>
+    /// Checks if a boolean condition is true.
+    /// </summary>
+    /// <param name="condition">The boolean condition to check.</param>
+    /// <returns>True if the condition is true, false otherwise.</returns>
     public static bool IsTrue(this bool? condition)
-        => condition is true;
+            => condition is true;
 }
