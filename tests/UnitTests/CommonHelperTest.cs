94c55f8223ef144816b53d9a6bcbb5bac904c81f
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-05-28 10:48:01 +0330

    Created `CommonHelpers` and added `IParseable<>` helpers

---
+++ tests/UnitTests/CommonHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Library.Globalization;
+
+using Xunit;
+
+namespace UnitTests;
+public sealed class CommonHelperTest
+{
+    [Fact]
+    public void Parse_ValidString_ReturnsParsedDateTime()
+    {
+        // Arrange
+        string input = "1399/01/01";
+        PersianDateTime expectedDateTime = new PersianDateTime(1399, 1, 1);
+
+        // Act
+        PersianDateTime result = input.Parse<PersianDateTime>();
+
+        // Assert
+        Assert.Equal(expectedDateTime, result);
+    }
+
+    [Fact]
+    public void Parse_InvalidString_ThrowsFormatException()
+    {
+        // Arrange
+        string input = "invalid_date_string";
+
+        // Act & Assert
+        Assert.Throws<FormatException>(() => input.Parse<PersianDateTime>());
+    }
+}
