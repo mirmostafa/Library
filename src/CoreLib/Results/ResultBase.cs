009edaca4fb88967878c581f782b52ad71d9bae8
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-05-30 00:48:26 +0330

    CoreLib is upgraded to C# 12. `Result` class families are full documented.

--- src/CoreLib/Results/ResultBase.cs
+++ src/CoreLib/Results/ResultBase.cs
@@ -13,20 +13,41 @@ public abstract record ResultBase(in bool? Succeed = null,
                                   in IEnumerable<(object Id, object Error)>? Errors = null,
                                   in ImmutableDictionary<string, object>? ExtraData = null)
 {
+    /// <summary>
+    /// Checks if the operation was successful by checking the Succeed flag, Status and Errors.
+    /// </summary>
     public virtual bool IsSucceed => this.Succeed ?? ((this.Status is null or 0 or 200) && (!this.Errors?.Any() ?? true));
+
+    /// <summary>
+    /// Gets a value indicating whether the operation has failed.
+    /// </summary>
     public virtual bool IsFailure => !this.IsSucceed;
+
     public void Deconstruct(out bool isSucceed, out string message)
         => (isSucceed, message) = (this.IsSucceed, this.Message?.ToString() ?? string.Empty);
 
+    /// <summary>
+    /// Indicates whether the current object is equal to another object of the same type.
+    /// </summary>
+    /// <param name="other">An object to compare with this instance.</param>
+    /// <returns>A value that indicates the relative order of the objects being compared.</returns>
     public virtual bool Equals(ResultBase? other)
         => other is not null && this.Status == other.Status;
 
+    /// <summary>
+    /// Serves as the default hash function.
+    /// </summary>
+    /// <returns>The hash code for the current instance.</returns>
     public override int GetHashCode()
         => HashCode.Combine(this.Status, this.Message);
 
     public static implicit operator bool(ResultBase result)
         => result.NotNull().IsSucceed;
 
+    /// <summary>
+    /// Generates a string representation of the result object.
+    /// </summary>
+    /// <returns>A string representation of the result object.</returns>
     public override string ToString()
     {
         var result = new StringBuilder();
@@ -53,6 +74,11 @@ public abstract record ResultBase(in bool? Succeed = null,
     private string GetDebuggerDisplay()
         => this.ToString();
 
+    /// <summary>
+    /// Combines multiple ResultBase objects into a single ResultBase object.
+    /// </summary>
+    /// <param name="results">The ResultBase objects to combine.</param>
+    /// <returns>A ResultBase object containing the combined results.</returns>
     protected static (bool? Succeed, object? Status, string? Message, IEnumerable<(object Id, object Error)>? Errors, ImmutableDictionary<string, object>? ExtraData) Combine(params ResultBase[] results)
     {
         bool? isSucceed = results.All(x => x.Succeed == null) ? null : results.All(x => x.IsSucceed);
@@ -85,10 +111,23 @@ public record Result(in bool? Succeed = null,
     private static Result? _empty;
     private static Result? _fail;
     private static Result? _success;
+
+    /// <summary>
+    /// Gets an empty result.
+    /// </summary>
+    /// <returns>An empty result.</returns>
     public static Result Empty => _empty ??= NewEmpty();
 
+    /// <summary>
+    /// Gets a Result object representing a failed operation.
+    /// </summary>
+    /// <returns>A Result object representing a failed operation.</returns>
     public static Result Failure => _fail ??= CreateFailure();
 
+    /// <summary>
+    /// Get a new instance of the Result class representing a successful operation.
+    /// </summary>
+    /// <returns>A new instance of the Result class representing a successful operation.</returns>
     public static Result Success => _success ??= CreateSuccess();
 
     /// <summary>
@@ -101,12 +140,28 @@ public record Result(in bool? Succeed = null,
     /// <returns>A new Result object with a failure status.</returns>
     public static Result CreateFailure(in object? status = null, in string? message = null, in IEnumerable<(object Id, object Error)>? errors = null, in ImmutableDictionary<string, object>? extraData = null)
         => new(false, status, message, Errors: errors, extraData);
+
+    /// <summary>
+    /// Creates a new Result object with a failure status and the specified message and errors.
+    /// </summary>
+    /// <param name="message">The message associated with the failure.</param>
+    /// <param name="errors">The errors associated with the failure.</param>
+    /// <returns>A new Result object with a failure status and the specified message and errors.</returns>
     public static Result CreateFailure(in string? message, in IEnumerable<(object Id, object Error)>? errors)
         => new(false, null, message, Errors: errors);
 
+    /// <summary>
+    /// Creates a new Result object with a success status.
+    /// </summary>
+    /// <param name="status">Optional status object.</param>
+    /// <param name="message">Optional message string.</param>
+    /// <returns>A new Result object with a success status.</returns>
     public static Result CreateSuccess(in object? status = null, in string? message = null)
         => new(true, status, message);
 
+    /// <summary>
+    /// Creates a new empty Result object.
+    /// </summary>
     public static Result NewEmpty()
         => new();
 
@@ -120,12 +175,28 @@ public record Result(in bool? Succeed = null,
         return result;
     }
 
+    /// <summary>
+    /// Creates a new Result object with a failure status and the specified message and error.
+    /// </summary>
+    /// <param name="message">The message to be included in the Result object.</param>
+    /// <param name="error">The Exception to be included in the Result object.</param>
+    /// <returns>A new Result object with a failure status and the specified message and error.</returns>
     public static Result CreateFailure(in string message, in Exception error)
         => CreateFailure(error, message);
 
+    /// <summary>
+    /// Creates a failure result with the given exception and optional message.
+    /// </summary>
+    /// <param name="error">The exception to use for the failure result.</param>
+    /// <returns>A failure result with the given exception.</returns>
     public static Result CreateFailure(Exception error)
         => CreateFailure(error, null);
 
+    /// <summary>
+    /// Combines multiple Result objects into a single Result object.
+    /// </summary>
+    /// <param name="results">The Result objects to combine.</param>
+    /// <returns>A single Result object containing the combined data.</returns>
     public static Result Combine(params Result[] results)
     {
         var data = ResultBase.Combine(results);
@@ -145,6 +216,11 @@ public record Result<TValue>(in TValue Value,
     , IEquatable<Result<TValue>>
 {
     private static Result<TValue?>? _failure;
+
+    /// <summary>
+    /// Gets a Result object representing a failed operation.
+    /// </summary>
+    /// <returns>A Result object representing a failed operation.</returns>
     public static Result<TValue?> Failure => _failure ??= CreateFailure();
 
     /// <summary>
@@ -174,6 +250,14 @@ public record Result<TValue>(in TValue Value,
         in ImmutableDictionary<string, object>? extraData = null)
         => new(value, false, status, message, errors, extraData);
 
+    /// <summary>
+    /// Creates a new Result with the given value, status, message, and error.
+    /// </summary>
+    /// <param name="value">The value of the Result.</param>
+    /// <param name="status">The status of the Result.</param>
+    /// <param name="message">The message of the Result.</param>
+    /// <param name="error">The error of the Result.</param>
+    /// <returns>A new Result with the given value, status, message, and error.</returns>
     public static Result<TValue?> CreateFailure(in TValue? value,
         in object? status,
         in string? message,
@@ -206,6 +290,14 @@ public record Result<TValue>(in TValue Value,
         return result;
     }
 
+    public static implicit operator TValue(Result<TValue> result)
+        => result.Value;
+
+    /// <summary>
+    /// Combines multiple Result<TValue> objects into a single Result<TValue> object.
+    /// </summary>
+    /// <param name="results">The Result<TValue> objects to combine.</param>
+    /// <returns>A single Result<TValue> object containing the combined results.</returns>
     public static Result<TValue> Combine(params Result<TValue>[] results)
     {
         var data = ResultBase.Combine(results);
@@ -213,15 +305,24 @@ public record Result<TValue>(in TValue Value,
         return result;
     }
 
-    public static implicit operator TValue(Result<TValue> result)
-        => result.Value;
     public void Deconstruct(out bool isSucceed, out TValue Value)
         => (isSucceed, Value) = (this.IsSucceed, this.Value);
+
+    /// <summary>
+    /// Converts a Result<TValue> to a Result.
+    /// </summary>
     public Result ToResult(in Result<TValue> result)
         => result;
     public static Result<TValue> From(in Result result, in TValue value)
         => new(value, result.Succeed, result.Status, result.Message, result.Errors, result.ExtraData);
 
+    /// <summary>
+    /// Creates a failure result with the specified message, exception and value.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="ex">The exception.</param>
+    /// <param name="value">The value.</param>
+    /// <returns>A failure result.</returns>
     public static Result<TValue?> CreateFailure(in string message, in Exception ex, in TValue? value)
         => CreateFailure(value, ex, message);
 
@@ -253,8 +354,16 @@ public record Result<TValue>(in TValue Value,
     public Result<TValue> WithError(params (object Id, object Error)[] errors)
         => this with { Errors = errors };
 
+    /// <summary>
+    /// Converts the current Result object to an asynchronous Task.
+    /// </summary>
     public Task<Result<TValue>> ToAsync()
         => Task.FromResult(this);
+
+    /// <summary>
+    /// Gets the value of the current instance.
+    /// </summary>
+    /// <returns>The value of the current instance.</returns>
     public TValue GetValue()
         => this.Value;
 }
