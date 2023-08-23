be60840e7d2db4fea4d7ebc0b27f137dfbd46961
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-08-23 17:39:32 +0330

    Working on saving auto-generated functionality codes to disk, automatically.

--- src/CoreLib/CodeGeneration/Models/Language.cs
+++ src/CoreLib/CodeGeneration/Models/Language.cs
@@ -1,7 +1,10 @@
-﻿using Library.Validations;
+﻿using System.Diagnostics;
+
+using Library.Validations;
 
 namespace Library.CodeGeneration.Models;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public readonly struct Language(in string name, in string? fileExtension = null) : IEquatable<Language>
 {
     public string? FileExtension { get; init; } = fileExtension;
@@ -12,12 +15,18 @@ public readonly struct Language(in string name, in string? fileExtension = null)
 
     public static bool operator ==(Language left, Language right) => left.Equals(right);
 
-    public override bool Equals(object? obj)
-        => obj is Language language && this.Equals(language);
+    public override bool Equals(object? obj) =>
+        obj is Language language && this.Equals(language);
 
-    public bool Equals(Language other)
-        => this.Name == other.Name && this.FileExtension == other.FileExtension;
+    public bool Equals(Language other) =>
+        this.Name == other.Name && this.FileExtension == other.FileExtension;
 
-    public override int GetHashCode()
-        => HashCode.Combine(this.Name, this.FileExtension);
+    public override int GetHashCode() =>
+        HashCode.Combine(this.Name, this.FileExtension);
+
+    public override string ToString() =>
+        this.Name;
+
+    private string GetDebuggerDisplay() =>
+        this.ToString();
 }
