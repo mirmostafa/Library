1655efde3eecab28d3f73c9882e83e5fa8e6fd85
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-09-12 11:30:06 +0330

    ایجاد دستی کلاس Converter و بخش utilities.  اضافه کردن کلاس Converter به IoC.

--- src/WebLib/Middlewares/InterceptorMiddleware.cs
+++ src/WebLib/Middlewares/InterceptorMiddleware.cs
@@ -6,21 +6,15 @@ using Library.Web.Middlewares.Markers;
 namespace Library.Web.Middlewares;
 
 [ContentGeneratorMiddleware]
-public sealed class InterceptorMiddleware : InfraMiddlewareBase
+public sealed class InterceptorMiddleware(RequestDelegate next) : InfraMiddlewareBase(next)
 {
-    public InterceptorMiddleware(RequestDelegate next)
-        : base(next)
-    {
-        
-    }
-
     protected override async Task OnExecutingAsync(ItemActingEventArgs<HttpContext?> args)
     {
         var httpContext = args.Item;
         var path = httpContext?.Request.Path.ToString().Trim().Trim('/').ToLower();
-        if (path?.EndsWith("/infra") is true)
+        if (path?.EndsWith("/infra") is true && httpContext is { } http)
         {
-            await httpContext?.Response.WriteAsync("<h1><center>MES Infrastructure is up.</center></h1>", Encoding.UTF8);
+            await http.Response.WriteAsync("<h1><center>MES Infrastructure is up.</center></h1>", Encoding.UTF8);
         }
     }
 }
