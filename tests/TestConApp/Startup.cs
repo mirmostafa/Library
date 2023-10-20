0f1430a83185575b66de0a1e1ce4e5b27234a35a
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2023-10-21 00:21:43 +0330

    Roslyn Code Generator is done.

--- tests/TestConApp/Startup.cs
+++ tests/TestConApp/Startup.cs
@@ -1,33 +1,45 @@
 ﻿using Library.CodeGeneration;
+using Library.Helpers.CodeGen;
 
-using Microsoft.CodeAnalysis.CSharp;
+Initialize();
 
-using TestConApp;
+var setAgeMethodBody = @"
+     this._age = age;
+     return this;
+     ";
+var ctorBody = @$"
+     this.Name = name;
+     this.{TypeMemberNameHelper.ToFieldName("age")} = age;
+     ";
+var ns = RoslynHelper.CreateNamespace("Test.Dtos");
+var personType = new TypePath("PersonDto");
+var personDto = RoslynHelper.CreateType(personType)
+    .AddField(new(TypeMemberNameHelper.ToFieldName("lastName"), TypePath.New<string>()), out var lastNameField)
+    .AddConstructor(new[] { (TypePath.New<string>(), "name"), (TypePath.New<int>(), "age") }, ctorBody)
+    .AddProperty<string>("Name")
+    .AddPropertyWithBackingField(new("LastName", TypePath.New<string>()), lastNameField)
+    .AddPropertyWithBackingField(new("Age", TypePath.New<int>(), setAccessor: (false, null)))
+    .AddMethod(new MethodInfo(null, personType, "SetAge", new[] { (TypePath.New<int>(), "age") }, setAgeMethodBody))
+    ;
 
-internal class Program
+ns = ns.AddType(personDto);
+
+var root = RoslynHelper.CreateRoot()
+    .AddUsingNameSpace("System")
+    .AddUsingNameSpace("System.Linq")
+    .AddUsingNameSpace("System.Threading")
+    .AddNameSpace(ns);
+WriteLine(root.GenerateCode());
+
+internal class RoslynTest
 {
-    private static void Main(string[] args)
+    public static string GenerateDtoCode(string className, params (TypePath Type, string Name)[] props)
     {
-        Initialize();
-
-        var setAgeMethodBody = @"
-this._year = year;
-return this;
-";
-        var ns = RoslynHelper.CreateNamespace("Test.Dtos");
-        var personType = new TypePath("PersonDto");
-        var personDto = RoslynHelper.CreateType(personType)
-            .AddField(new("_lastName", TypePath.New<int>()), out var lastNameField)
-            .AddConstructor()
-            .AddProperty<string>("Name")
-            .AddPropertyWithBackingField(new("LastName", TypePath.New<string>()), lastNameField)
-            .AddPropertyWithBackingField(new("Age", TypePath.New<int>(), setAccessor: (false, null)))
-            .AddProperty(new PropertyInfo("StaticProperty", TypePath.New<int>(), modifiers: new[] { SyntaxKind.PublicKeyword, SyntaxKind.StaticKeyword }))
-            .AddMethod(new MethodInfo(null, personType, "SetAge", new[] { (TypePath.New<int>(), "age") }, setAgeMethodBody))
-            ;
-
-        ns = ns.AddType(personDto);
-
-        WriteLine(ns.GenerateCode());
+        var result = RoslynHelper.CreateType(className);
+        foreach (var prop in props)
+        {
+            result = result.AddProperty(prop.Name, prop.Type);
+        }
+        return result.GenerateCode();
     }
 }
---
+++ tests/TestConApp/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Test.Dtos
+{
+    public sealed class PersonDto
+    {
+        private string _lastName;
+        public PersonDto(string name, int age)
+        {
+            this.Name = name;
+            this._age = age;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+        }
+
+        public PersonDto SetAge(int age)
+        {
+            this._age = age;
+            return this;
+        }
+    }
+}
