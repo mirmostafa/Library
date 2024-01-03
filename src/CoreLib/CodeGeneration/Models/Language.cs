9171b0866a60e5ad9b77129387671a7561b68218
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-01-03 10:14:28 +0330

    Checkpoint: Restart after 2 days

--- src/CoreLib/CodeGeneration/Models/Language.cs
+++ src/CoreLib/CodeGeneration/Models/Language.cs
@@ -1,30 +1,15 @@
 ﻿using System.Diagnostics;
 
+using Library.DesignPatterns.Markers;
 using Library.Types;
 using Library.Validations;
 
 namespace Library.CodeGeneration.Models;
 
 [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-public readonly struct Language(in string name, in string? fileExtension = null) : IEquatable<Language>
+[Immutable]
+public readonly record struct Language([DisallowNull]in string Name, in string? FileExtension = null) : IEquatable<Language>
 {
-    public string? FileExtension { get; init; } = fileExtension;
-
-    public string Name { get; init; } = name.ArgumentNotNull();
-
-    public static bool operator !=(Language left, Language right) => !(left == right);
-
-    public static bool operator ==(Language left, Language right) => left.Equals(right);
-
-    public override bool Equals(object? obj) =>
-        obj is Language language && this.Equals(language);
-
-    public bool Equals(Language other) =>
-        this.Name == other.Name && this.FileExtension == other.FileExtension;
-
-    public override int GetHashCode() =>
-        HashCode.Combine(this.Name, this.FileExtension);
-
     public override string ToString() =>
         this.Name;
 
