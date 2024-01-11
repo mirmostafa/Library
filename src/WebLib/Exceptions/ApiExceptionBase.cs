82c1bb9b96ea807c4732d2fcb956f8887e2fc1f6
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-01-11 20:31:12 +0330

    Nugets are updated

--- src/WebLib/Exceptions/ApiExceptionBase.cs
+++ src/WebLib/Exceptions/ApiExceptionBase.cs
@@ -1,24 +1,31 @@
-﻿using Library.Exceptions;
-
-using System.Net;
+﻿using System.Net;
 using System.Web.Http;
+
+using Library.Exceptions;
 
 namespace Library.Web.Exceptions;
 
 public abstract class ApiExceptionBase : HttpResponseException, IApiException
 {
+    protected ApiExceptionBase(HttpResponseMessage message) : base(message) => 
+        this.Message = message?.ToString() ?? string.Empty;
+
     protected ApiExceptionBase(int? statusCode = null, string? message = null, object? owner = null)
-        : base(new((HttpStatusCode?)statusCode ?? HttpStatusCode.BadRequest))
+        : base((HttpStatusCode?)statusCode ?? HttpStatusCode.BadRequest)
     {
         this.Message = message ?? string.Empty;
         this.StatusCode = statusCode;
         this.Owner = owner;
     }
+
+    public string? Details { get; set; }
+
+    string? IException.Instruction { get; }
+
     /// <inheritdoc/>
     public override string Message { get; }
-    public int? StatusCode { get; }
-    string? IException.Instruction { get; }
+
     public object? Owner { get; set; }
+    public int? StatusCode { get; }
     public string? Title { get; }
-    public string? Details { get; set; }
 }
