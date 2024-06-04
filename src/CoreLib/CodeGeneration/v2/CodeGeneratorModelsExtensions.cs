752b82d82a8477f865e300eee8c480a303b5e13c
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-06-04 12:23:19 +0330

    Working on Code Generator

--- src/CoreLib/CodeGeneration/v2/CodeGeneratorModelsExtensions.cs
+++ src/CoreLib/CodeGeneration/v2/CodeGeneratorModelsExtensions.cs
@@ -11,7 +11,7 @@ public static class CodeGeneratorModelsExtensions
     public static THasAttributes AddAttribute<THasAttributes>([DisallowNull] this THasAttributes model, TypePath attribute, params (string? Name, string Value)[] properties)
         where THasAttributes : IHasAttributes
     {
-        model.ArgumentNotNull().Attributes.Add(ICodeGenAttribute.New(TypePath.New(attribute), properties));
+        _ = model.ArgumentNotNull().Attributes.Add(ICodeGenAttribute.New(TypePath.New(attribute), properties));
         return model;
     }
 }
